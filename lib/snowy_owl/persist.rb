5e70f988ad33df7d7498aa39b320f3d3eff3f0ab
Yi Han <hanystudy@gmail.com>
2017-07-20 16:49:08 +0800

    [hy] fix persist path issue

--- lib/snowy_owl/persist.rb
+++ lib/snowy_owl/persist.rb
@@ -8,16 +8,17 @@ module SnowyOwl
     class << self
       def persist_state(plot_name)
         plot_name_path = SnowyOwl::Support.to_underscore plot_name
-        path = SnowyOwl.persist_path + '/.tmp/' + plot_name_path
+        temp_persist_path = SnowyOwl.persist_path + '/.tmp/'
+        path = temp_persist_path + plot_name_path
         FileUtils::mkdir_p path
-        args = [plot_name_path, path]
+        args = [plot_name_path, temp_persist_path]
         SnowyOwl.persist_callback.call(args)
       end
 
       def recover_state(plot_name)
         plot_name_path = SnowyOwl::Support.to_underscore plot_name
         path = SnowyOwl.persist_path + plot_name_path
-        args = [plot_name_path, path]
+        args = [plot_name_path, SnowyOwl.persist_path]
         SnowyOwl.recover_callback.call(args)
       end
     end
